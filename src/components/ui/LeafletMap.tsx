b965e44786ccb0398b8144596172261817280de5
rob-pasc <robinpascal.k@gmail.com>
2026-04-09 11:59:11 +0200

    Improve pre-fetch logic

--- src/components/ui/LeafletMap.tsx
+++ src/components/ui/LeafletMap.tsx
@@ -77,6 +77,7 @@ export default function LeafletMap({ projects, onSelectProject, selectedId }: Le
       <TileLayer
         url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+        subdomains="abcd"
       />
       <BoundsFitter projects={visibleProjects} />
       <MapResizer selectedId={selectedId} />
